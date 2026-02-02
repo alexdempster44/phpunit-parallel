6a7b9f97d26e0867295f9cf548cd4949495383f3
Alex Dempster <adgalex44@gmail.com>
2026-02-02 11:42:11 +0000

    feat: cli arguments

--- cmd/root.go
+++ cmd/root.go
@@ -6,9 +6,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	configFile string
+	teamcity   bool
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "phpunit-parallel",
 	Short: "Run PHPUnit tests in parallel",
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: implement
+	},
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&configFile, "configuration", "c", "phpunit.xml", "PHPUnit configuration file")
+	rootCmd.Flags().BoolVar(&teamcity, "teamcity", false, "Output in TeamCity format")
 }
 
 func Execute() {
